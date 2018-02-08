3ac9a17cb972cc5a3daa137b1b9f64ac40bd5164
akayunov <akayunov@yandex.ru>
2018-02-08 22:12:48 +0300

    'qwe'

--- src/sc2/keyeventcommand.py
+++ src/sc2/keyeventcommand.py
@@ -12,7 +12,7 @@ class KeyEventCommand:
         self.map_info = map_info
 
         self.hotkey_occupy_expand = 'z+x'
-        self.hotkey_build_svc = 'z+c'
+        self.hotkey_build_svc = 'z+m'
         # TODO don't need two hot key for unit and for tanks
         self.hotkey_send_command_to_units_by_one = '`'
         self.hotkey_send_command_to_units_by_one_for_resiege_tanks = 'w'
@@ -21,9 +21,7 @@ class KeyEventCommand:
         self.scv_building_time = 12
         self.threads_flags = {
             self.hotkey_send_command_to_units_by_one: [],
-            self.hotkey_occupy_expand: [],
             self.hotkey_send_command_to_units_by_one_for_resiege_tanks: [],
-            self.hotkey_build_svc: [],
         }
         keyboard.on_release(self.turn_off_threads_flags)
 
@@ -41,89 +39,80 @@ class KeyEventCommand:
         )
 
     def turn_off_threads_flags(self, event):
-        if (event.name == 'x' and keyboard.is_pressed('z')) or (event.name == 'z' and keyboard.is_pressed('x')):
-            self.threads_flags[self.hotkey_occupy_expand] = []
-        if (event.name == 'c' and keyboard.is_pressed('z')) or (event.name == 'z' and keyboard.is_pressed('c')):
-            self.threads_flags[self.hotkey_build_svc] = []
         if event.name == self.hotkey_send_command_to_units_by_one:
             self.threads_flags[self.hotkey_send_command_to_units_by_one] = []
         if event.name == self.hotkey_send_command_to_units_by_one_for_resiege_tanks:
             self.threads_flags[self.hotkey_send_command_to_units_by_one_for_resiege_tanks] = []
 
     def occupy_expand(self, hotkey):
-        # recreate thread for next run
-        # threading.get_ident() can be recycled by new thread but is doesn't matter because we always reinitialize it
-        self.threads_flags[hotkey].append(threading.get_ident())
-        keyboard.remove_hotkey(hotkey)
-        keyboard.add_hotkey(hotkey, threading.Thread(target=self.occupy_expand, args=(hotkey,)).start)
-
-        mouse_position_x, mouse_position_y = mouse.get_position()
-        resourses_group = self.map_info.get_nearest_exp_resourses_group(mouse_position_x, mouse_position_y)
-        new_mouse_position_x, new_mouse_position_y = self.map_info.calculate_main_building_position(resourses_group)
-        mouse.move(new_mouse_position_x, new_mouse_position_y)
-        mouse.click(button='left')  # move by minimap to center of screen on new expand
-        keyboard.send('0')  # choose worker
-        time.sleep(0.1)  # some exerimental things
-        mouse.move(RESOLUTION.x / 2, RESOLUTION.y / 2)
-        keyboard.send('b,c')
-
-        mouse.wait()  # build cc
-        if not threading.get_ident() in self.threads_flags[hotkey]:
-            return
-
-        # TODO move worker which build cc to minerals
-        for i, gaz in enumerate(resourses_group['gazes']):
-            new_mouse_position_x, new_mouse_position_y = self.map_info.get_item_coordinate_on_whole_screen(gaz)
-            time.sleep(0.1)
-            mouse.move(new_mouse_position_x, new_mouse_position_y)  # move by minimap
-            time.sleep(0.1)
-            mouse.click()
+        try:
+            keyboard.remove_hotkey(hotkey)
+            mouse_position_x, mouse_position_y = mouse.get_position()
+            resourses_group = self.map_info.get_nearest_exp_resourses_group(mouse_position_x, mouse_position_y)
+            new_mouse_position_x, new_mouse_position_y = self.map_info.calculate_main_building_position(resourses_group)
+            mouse.move(new_mouse_position_x, new_mouse_position_y)
+            mouse.click(button='left')  # move by minimap to center of screen on new expand
             keyboard.send('0')  # choose worker
-            time.sleep(0.1)
+            time.sleep(0.1)  # some exerimental things
             mouse.move(RESOLUTION.x / 2, RESOLUTION.y / 2)
-            keyboard.send('b,r')
+            keyboard.send('b,c')
+            mouse.wait()  # build cc
+            keyboard.send('esc')
 
-            mouse.wait()  # build gaz
-            if not threading.get_ident() in self.threads_flags[hotkey]:
-                return
+            # TODO move worker which build cc to minerals
+            for i, gaz in enumerate(resourses_group['gazes']):
+                new_mouse_position_x, new_mouse_position_y = self.map_info.get_item_coordinate_on_whole_screen(gaz)
+                time.sleep(0.1)
+                mouse.move(new_mouse_position_x, new_mouse_position_y)  # move by minimap
+                time.sleep(0.1)
+                mouse.click()
+                keyboard.send('0')  # choose worker
+                time.sleep(0.1)
+                mouse.move(RESOLUTION.x / 2, RESOLUTION.y / 2)
+                keyboard.send('b,r')
+                mouse.wait()  # build gaz
+                keyboard.send('esc')
+        finally:
+            keyboard.send('esc')  # if I want to built not all building I need to pass first and send esc to second and third worker
+            # recreate thread for next run
+            keyboard.add_hotkey(hotkey, threading.Thread(target=self.occupy_expand, args=(hotkey,)).start)
 
     def build_scv(self, hotkey):
-        # recreate thread for next run
-        # threading.get_ident() can be recycled by new thread but is doesn't matter because we always reinitialize it
-        self.threads_flags[hotkey].append(threading.get_ident())
-        keyboard.remove_hotkey(hotkey)
-        keyboard.add_hotkey(hotkey, threading.Thread(target=self.build_scv, args=(hotkey,)).start)
+        try:
+            keyboard.remove_hotkey(hotkey)
+            worker_counter = 22
 
-        worker_counter = 22
+            while 1:
+                # TODO I don't need screenshot to check building quue just now how many cc on map by parsing
+                # 6 group key and that all and d oorder after 12 sec
+                # as close as posible to send "s" but before get screenshot, do I realy need scren shot I just need to know how many cc is exits
+                image = get_screenshot()
+                try:
+                    #  check resourses before order to ensure that order will be done
+                    sasayblock = SasayBlock()
+                    sasayblock.parse_regions(image)
+                    worker_can_be_order_by_this_mineral_count = int(sasayblock.minerals / 50)
+                except Exception:
+                    worker_can_be_order_by_this_mineral_count = 4
 
-        while 1:
-            # TODO I don't need screenshot to check building quue just now how many cc on map by parsing
-            # 6 group key and that all and d oorder after 12 sec
-            # as close as posible to send "s" but before get screenshot, do I realy need scren shot I just need to know how many cc is exits
-            image = get_screenshot()
-            try:
-                #  check resourses before order to ensure that order will be done
-                sasayblock = SasayBlock()
-                sasayblock.parse_regions(image)
-                worker_can_be_order_by_this_mineral_count = int(sasayblock.minerals / 50)
-            except Exception:
-                worker_can_be_order_by_this_mineral_count = 4
+                # TODO how to disable right click in this time?
+                worker_needed = 2  # TODO get this number from parsing cc count
 
-            # TODO how to disable right click in this time?
-            worker_needed = 2  # TODO get this number from parsing cc count
-
-            keyboard.send('ctrl+9')  # add curent selected to 0, for returning back
-            keyboard.send('6')
-            for i in range(min(worker_can_be_order_by_this_mineral_count, worker_needed)):
-                worker_counter -= 1
-                keyboard.send('s')
+                keyboard.send('ctrl+9')  # add curent selected to 0, for returning back
+                keyboard.send('6')
+                for i in range(min(worker_can_be_order_by_this_mineral_count, worker_needed)):
+                    worker_counter -= 1
+                    keyboard.send('s')
+                    if worker_counter <= 0:
+                        break
                 if worker_counter <= 0:
                     break
-            if worker_counter <= 0:
-                break
-            keyboard.send('9,9')
-            time.sleep(self.scv_building_time)
-        keyboard.send('9,9')
+                keyboard.send('9')
+                time.sleep(self.scv_building_time)
+            keyboard.send('9')
+        finally:
+            # recreate thread for next run
+            keyboard.add_hotkey(hotkey, threading.Thread(target=self.build_scv, args=(hotkey,)).start)
 
     @staticmethod
     def move_unit_command():
@@ -139,26 +128,28 @@ class KeyEventCommand:
     def send_command_to_units_by_one(self, hotkey, command):
         # recreate thread for next run
         # threading.get_ident() can be recycled by new thread but is doesn't matter because we always reinitialize it
-        self.threads_flags[hotkey].append(threading.get_ident())
-        keyboard.remove_hotkey(hotkey)
-        keyboard.add_hotkey(hotkey, threading.Thread(target=self.send_command_to_units_by_one, args=(hotkey, command)).start)
+        try:
+            self.threads_flags[hotkey].append(threading.get_ident())
+            keyboard.remove_hotkey(hotkey)
 
-        # move units to group 9
-        keyboard.send('ctrl+9')
-        while threading.get_ident() in self.threads_flags[hotkey]:
-            mouse.wait(button='left', target_types=('up',))
-            if not threading.get_ident() in self.threads_flags[hotkey]:
-                break
-            mouse_position_x, mouse_position_y = mouse.get_position()
+            # move units to group 9
+            keyboard.send('ctrl+9')
+            while threading.get_ident() in self.threads_flags[hotkey]:
+                mouse.wait(button='left', target_types=('up',))
+                if not threading.get_ident() in self.threads_flags[hotkey]:
+                    break
+                mouse_position_x, mouse_position_y = mouse.get_position()
 
-            # click on first unit
-            mouse.move(700, 900)
-            mouse.click()
+                # click on first unit
+                mouse.move(700, 900)
+                mouse.click()
 
-            # remove unit from group
-            keyboard.send('alt+8')
+                # remove unit from group
+                keyboard.send('alt+8')
 
-            mouse.move(mouse_position_x, mouse_position_y)
-            command()
-            # choose remaining units
-            keyboard.send('9')
+                mouse.move(mouse_position_x, mouse_position_y)
+                command()
+                # choose remaining units
+                keyboard.send('9')
+        finally:
+            keyboard.add_hotkey(hotkey, threading.Thread(target=self.send_command_to_units_by_one, args=(hotkey, command)).start)
